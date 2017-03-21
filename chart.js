7fc0b04fd418a3d07147194a660d1492f35a58ea
fhaque1 <farhan3231@gmail.com>
2017-03-20 22:18:13 -0400

    color changes

--- chart.js
+++ chart.js
@@ -4,21 +4,31 @@ var data_2016 = [2.99, 3.54, -552, -3.3, 16.5];
 
 
 var makeBars = function(info) {
-    
     dyn.selectAll("div")
 	.data(info)
 	.enter()
 	.append("div")
 	.style("width", function(d) {
+		if (d < 0){
+			return d * -10 + "px";
+		}	
 	    return d * 10+ "px";
 	})
 	.text( function(d) {
+		if (d < 0){
+			return d * -1 + "";
+		}	
 	    return d;
+	})
+	.style("background-color", function(d) { 
+							if (d<0){
+								return "red"
+							} 
+							return "green";
 	});
 };
 
 var transitionTest = function(info) {
-    
        dyn.selectAll("div")
 	.data(info)
 	.transition()
@@ -27,9 +37,19 @@ var transitionTest = function(info) {
 	    return d * 10 + "px";
 	  });
 };
+
+var oNine = function(){
+	makeBars(data_2009);
+	transitionTest(data_2009);
+};
+
+var sixTeen = function(){
+	makeBars(data_2016);
+	transitionTest(data_2016);
+};
 	 
-makeBars(data_2009);
+
 var c09 = document.getElementById("2009");
-c09.addEventListener("click", transitionTest(data_2009));
+c09.addEventListener("click", oNine);
 var c16 = document.getElementById("2016");
-c16.addEventListener("click", transitionTest(data_2016));
+c16.addEventListener("click", sixTeen);
